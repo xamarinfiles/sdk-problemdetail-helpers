5dfec8a723c19a015fb3463712c7329054816362
Andy Lech <github@andylech.com>
2023-07-16 17:11:40 -0400

    Move JSON-friendly ProblemDetails variants to new ProblemReport classes

---
+++ PdHelpers.Refit/Converters.cs
@@ -0,0 +1,28 @@
+﻿using XamarinFiles.PdHelpers.Refit.Enums;
+using XamarinFiles.PdHelpers.Refit.Models;
+using static XamarinFiles.PdHelpers.Refit.Bundlers;
+using RefitProblemDetails = Refit.ProblemDetails;
+
+namespace XamarinFiles.PdHelpers.Refit
+{
+    public static class Converters
+    {
+        public static ProblemReport
+            ConvertFromProblemDetails(DetailsVariant detailsVariant,
+                RefitProblemDetails problemDetails, string[] developerMessages,
+                string[] userMessages, ExceptionMessages exceptionMessages)
+        {
+            var problemReport = BundleProblemReport(detailsVariant,
+                problemDetails.Status,
+                problemDetails.Title,
+                problemDetails.Detail,
+                problemDetails.Instance,
+                problemDetails.Type,
+                developerMessages,
+                userMessages,
+                exceptionMessages);
+
+            return problemReport;
+        }
+    }
+}
